a24d2914f7946e80d14f87a4dba9e24a17657ba3
Yan Mendes <ymendesf@gmail.com>
2020-10-30 00:09:23 -0300

    fix: grant bucket objects public permission

--- resources/main.tf
+++ resources/main.tf
@@ -25,9 +25,24 @@ terraform {
   required_version = "0.12.28"
 }
 
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    sid = "PublicReadAccess"
+    actions = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::shappables-image-bucket/*"]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+
 resource "aws_s3_bucket" "image-bucket" {
   bucket = "shappables-image-bucket"
-  acl    = "private"
+  acl    = "public-read"
+  policy = data.aws_iam_policy_document.bucket_policy.json
 
   tags = local.tags
 }
