2cf339c3eea676b5ee045ede9ef642b327c446fc
Yan Mendes <ymendesf@gmail.com>
2020-11-02 18:00:17 -0300

    feat: sync elastic search index

--- packages/server/jestSetup.js
+++ packages/server/jestSetup.js
@@ -1,6 +1,16 @@
-import { db } from './src/services'
+import { Client } from '@elastic/elasticsearch'
 
-beforeAll(() => db.sync())
+import { db } from './src/services'
+import { esEndpoint, esIndex } from './src/config'
+
+const esClient = new Client({ node: esEndpoint })
+beforeEach(async () => {
+  await db.sync()
+  const { body: indiceExists } = await esClient.indices.exists({
+    index: esIndex
+  })
+  if (!indiceExists) await esClient.indices.create({ index: esIndex })
+})
 
 // TODO: Clean up elasticsearch
 afterAll(() => db.close())
--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,15 +1,20 @@
+import { Client } from '@elastic/elasticsearch'
+
 import logger from './logger'
-import { port } from './config'
+import { port, esEndpoint, esIndex } from './config'
 import app from './server'
 import db from './services/db'
 
 const category = 'server_setup'
 const log = logger.child({ category })
+const esClient = new Client({ node: esEndpoint })
 
 ;(async () => {
   try {
     logger.debug('Connecting to the database...')
     await db.sync()
+    const { body: indiceExists } = await esClient.indices.exists({ index: esIndex })
+    if (!indiceExists) await esClient.indices.create({ index: esIndex })
     app.listen({ port }, () =>
       logger.info(`🚀 Server ready at http://localhost:${port}`)
     )
