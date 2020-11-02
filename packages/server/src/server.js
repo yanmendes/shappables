cdab433279139abaec9f1eef53040207740dd6ef
Yan Mendes <ymendesf@gmail.com>
2020-11-02 20:14:14 -0300

    fix: enable client <> server comms

--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -2,12 +2,14 @@ import bodyParser from 'body-parser'
 import express from 'express'
 import pino from 'express-pino-logger'
 import fileUpload from 'express-fileupload'
+import cors from 'cors'
 
 import logger from './logger'
 import routes from './routes'
 import { maxFileSize } from './config'
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(pino({ logger }))
